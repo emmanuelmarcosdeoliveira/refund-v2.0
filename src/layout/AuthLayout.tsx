daf8d17166f20e6cdda7c93b7e7df8827deb16d1
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2025-03-07 00:06:53 -0300

    chore: added the loading component to the project

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -3,8 +3,8 @@ import logo from "../assets/logo.svg";
 
 export function AuthLayout() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-400 text-gray-100">
-      <main className="flex flex-col items-center rounded-md bg-gray-500 p-8 md:min-w-[462px]">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-400 p-8 text-gray-100">
+      <main className="flex w-full flex-col items-center rounded-md bg-gray-500 p-8 md:max-w-[462px]">
         <img className="my-8" src={logo} alt="Logo Refund | OFS" />
         <Outlet />
       </main>
