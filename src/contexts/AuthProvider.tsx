a71afe7fcc58a8aa17187ce8be2b95dbaacd7389
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-24 23:18:27 -0300

    feat(AuthProvider): adds user and token storage in localStorage

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { AuthContext } from "./AuthContext";
 
+const LOCAL_STORAGE_KEY = "@refund";
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(
+      `${LOCAL_STORAGE_KEY}:user`,
+      JSON.stringify(data.user),
+    );
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}: token`, data.token);
+
     setSession(data);
   }
 
